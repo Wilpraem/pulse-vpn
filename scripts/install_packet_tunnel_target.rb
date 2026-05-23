c1776a7aa85ebaac893b7f7f1f8cfe53ec9a826a
Andrey Pro <a@.a>
2026-05-23 17:46:40 +0300

    Add generated iOS native project

--- scripts/install_packet_tunnel_target.rb
+++ scripts/install_packet_tunnel_target.rb
@@ -14,6 +14,12 @@ extension_name = 'PacketTunnel'
 extension_target = project.targets.find { |target| target.name == extension_name }
 extension_target ||= project.new_target(:app_extension, extension_name, :ios, '16.0')
 
+if extension_target.product_reference
+  extension_target.product_reference.path = "#{extension_name}.appex"
+  extension_target.product_reference.name = "#{extension_name}.appex"
+  extension_target.product_reference.explicit_file_type = 'wrapper.app-extension'
+end
+
 extension_group = project.main_group.find_subpath('PacketTunnel', true)
 extension_group.set_source_tree('<group>')
 
@@ -23,14 +29,23 @@ source_files = %w[
 ]
 
 source_files.each do |relative_path|
-  file_ref = extension_group.files.find { |file| file.path == File.basename(relative_path) }
+  file_name = File.basename(relative_path)
+  file_refs = (extension_group.files + project.files).select { |file| file.path == relative_path || file.path == file_name || file.display_name == file_name }.uniq
+  file_ref = file_refs.find { |file| extension_target.source_build_phase.files_references.include?(file) } || file_refs.first
+  (file_refs - [file_ref]).each { |file| file.remove_from_project unless extension_target.source_build_phase.files_references.include?(file) }
   file_ref ||= extension_group.new_file(relative_path)
+  extension_group.children << file_ref unless extension_group.children.include?(file_ref)
   extension_target.add_file_references([file_ref]) unless extension_target.source_build_phase.files_references.include?(file_ref)
 end
 
 extension_target.build_configurations.each do |config|
   settings = config.build_settings
   settings['PRODUCT_BUNDLE_IDENTIFIER'] = 'com.pulsevpn.app.PacketTunnel'
+  settings['PRODUCT_NAME'] = extension_name
+  settings['PRODUCT_MODULE_NAME'] = extension_name
+  settings['WRAPPER_EXTENSION'] = 'appex'
+  settings['CURRENT_PROJECT_VERSION'] = '1'
+  settings['MARKETING_VERSION'] = '0.1.0'
   settings['INFOPLIST_FILE'] = 'PacketTunnel/Info.plist'
   settings['CODE_SIGN_ENTITLEMENTS'] = 'PacketTunnel/PacketTunnel.entitlements'
   settings['SWIFT_VERSION'] = '5.0'
